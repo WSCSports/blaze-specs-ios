7b00a7866f9727eb4716bbcc06a172bfccfdecd8
Yossi Elkrief <yossi.elkrief@wsc-sports.com>
2023-11-21 11:48:22 +0200

    Version 0.3.7

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     targets: [
       .binaryTarget(name: "BlazeSDK",
-                    url: "https://github.com/WSCSports/blaze-specs-ios/releases/download/0.3.2/BlazeSDK.zip",
-                    checksum: "cdb7b37e15b16d01bf9579bcc3e8e1b7418eb4a23c8aaeaccb9f8c89a187705c")
+                    url: "https://github.com/WSCSports/blaze-specs-ios/releases/download/0.3.7/BlazeSDK.zip",
+                    checksum: "5750691541f103f02ec6ceaff151668ed3101a3fc6cbd95cda4f15f0b0d61f40")
     ]
 )
