fa68c0b26c14cb675bfb181f1ac76f5b0abf3584
DorZafrir <dor@monkeytech.co.il>
2024-05-06 11:29:26 +0300

    Version 0.13.0

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     targets: [
       .binaryTarget(name: "BlazeSDK",
-                    url: "https://github.com/WSCSports/blaze-specs-ios/releases/download/0.12.0/BlazeSDK.zip",
-                    checksum: "d889f19b195cd030cc5ecbd5ac7742026f6ab6ae533de38474af85d277c42bba")
+                    url: "https://github.com/WSCSports/blaze-specs-ios/releases/download/0.13.0/BlazeSDK.zip",
+                    checksum: "f14a5635253f1ef5c664d0f9a10fa2535bab463c78159737683ba3ea452cec3a")
     ]
 )
