0ad87034e58eaafeb7c696dae80699423080a5ca
runner <runner@Mac-1730798979300.local>
2024-11-05 09:50:49 +0000

    Bump version to 1.3.0

--- Package.swift
+++ Package.swift
@@ -27,8 +27,8 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "BlazeSDK",
-                      url: "https://github.com/WSCSports/blaze-specs-ios/releases/download/1.2.5/BlazeSDK.zip",
-                      checksum: "c6d195fdc23f62bc1f3d512b5f5471d63de1e696975eadba8edf8c2650a57e11"),
+                      url: "https://github.com/WSCSports/blaze-specs-ios/releases/download/1.3.0/BlazeSDK.zip",
+                      checksum: "9fd6b9eba38a9c43590a11538fd590871c1f22e506a72654dcf6bfb34cad9d8a"),
         .target(name: "BlazeIMA",
                 dependencies: ["BlazeSDK",
                                .product(name: "GoogleInteractiveMediaAds",
